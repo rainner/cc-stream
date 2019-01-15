ee9222626372ee6512a203d9b284b082d1eeb8da
Rainner Lins <rainnerlins@gmail.com>
2019-01-15 03:35:11 -0500

    Added price conversion and other methods to coin class.

--- public/js/coin.js
+++ public/js/coin.js
@@ -17,10 +17,13 @@ class Coin {
     this.image = '';
     this.url = '';
     this.value = '';
+    this.isfav = 0;
     // global values
-    this.price = 0;
-    this.position = 0;
     this.rank = 0;
+    this.price = 0;
+    this.ath_price = 0;
+    this.converted_price = 0;
+    this.price_prefix = '$';
     this.volume_24h = 0;
     this.market_cap = 0;
     this.circulating_supply = 0;
@@ -34,10 +37,24 @@ class Coin {
     // change 7d
     this.percent_change_7d = 0;
     this.percent_change_7d_style = 'same';
+    // change 30d
+    this.percent_change_30d = 0;
+    this.percent_change_30d_style = 'same';
+    // change 1y
+    this.percent_change_1y = 0;
+    this.percent_change_1y_style = 'same';
     // graph data
+    this.graph_24h_url = '';
     this.graph_7d_url = '';
+    this.graph_30d_url = '';
+    this.graph_1y_url = '';
     this.graph_last = Date.now();
     this.graph_data = [];
+    // social data
+    this.twitter_subs = 0;
+    this.reddit_subs = 0;
+    this.github_subs = 0;
+    this.telegram_subs = 0;
   }
 
   /**
@@ -52,28 +69,72 @@ class Coin {
     this.symbol = String( this.symbol ).replace( /[^a-zA-Z]+/g, '' ).toUpperCase();
     this.quote = String( this.quote ).replace( /[^a-zA-Z]+/g, '' ).toUpperCase();
     this.pair = this.symbol + this.quote;
+    this.graph_24h_url = `https://graphs.coinpaprika.com/currency/chart/${ this.id }/24h/svg`;
+    this.graph_7d_url = `https://graphs.coinpaprika.com/currency/chart/${ this.id }/7d/svg`;
+    this.graph_30d_url = `https://graphs.coinpaprika.com/currency/chart/${ this.id }/30d/svg`;
+    this.graph_1y_url = `https://graphs.coinpaprika.com/currency/chart/${ this.id }/1y/svg`;
     this.resolveImage();
   }
 
   /**
    * Set price related ticker data
-   * @param {object}   data   Data object
+   * @param {object}  data   Data object
    */
   updateTicker( data ) {
     Object.assign( this, data );
     this.rank = parseInt( this.rank ) || 0;
     this.price = parseFloat( this.price ) || 0;
+    this.ath_price = parseFloat( this.ath_price ) || 0;
     this.volume_24h = parseFloat( this.volume_24h ) || 0;
     this.market_cap = parseFloat( this.market_cap ) || 0;
     this.circulating_supply = parseFloat( this.circulating_supply ) || 0;
     this.max_supply = parseFloat( this.max_supply ) || 0;
-    this.position = parseInt( this.position ) || 0;
     this.percent_change_1h = parseFloat( this.percent_change_1h ) || 0;
     this.percent_change_1h_style = this.calculateStyle( this.percent_change_1h );
     this.percent_change_24h = parseFloat( this.percent_change_24h ) || 0;
     this.percent_change_24h_style = this.calculateStyle( this.percent_change_24h );
     this.percent_change_7d = parseFloat( this.percent_change_7d ) || 0;
     this.percent_change_7d_style = this.calculateStyle( this.percent_change_7d );
+    this.percent_change_30d = parseFloat( this.percent_change_30d ) || 0;
+    this.percent_change_30d_style = this.calculateStyle( this.percent_change_30d );
+    this.percent_change_1y = parseFloat( this.percent_change_1y ) || 0;
+    this.percent_change_1y_style = this.calculateStyle( this.percent_change_1y );
+  }
+
+  /**
+   * Set social subs counts
+   * @param {object}  data   Data object
+   */
+  updateSubs( data ) {
+    Object.assign( this, data );
+    this.twitter_subs = parseInt( this.twitter_subs ) || 0;
+    this.reddit_subs = parseInt( this.reddit_subs ) || 0;
+    this.github_subs = parseInt( this.github_subs ) || 0;
+    this.telegram_subs = parseInt( this.telegram_subs ) || 0;
+  }
+
+  /**
+   * Apply price conversion
+   * @param {string}  symbol  Quote price symbol
+   * @param {number}  usdval  Quote prive value in usd
+   * @param {string}  prefix  Prefix symbol
+   */
+  convertPrice( symbol, usdval, prefix ) {
+    symbol = String( symbol || '' );
+    usdval = Number( usdval || 0 );
+    prefix = String( prefix || '' );
+
+    this.converted_price = ( symbol === 'USD' ) ? this.price : Number( ( 100000000 / usdval ) * this.price ) / 100000000;
+    this.price_prefix = prefix;
+  }
+
+  /**
+   * Used to toggle favorite status for a coin
+   * @param {boolean}  toggle  Boolean toggle
+   */
+  setFavorite( toggle ) {
+    if ( typeof toggle !== 'boolean' ) return;
+    this.isfav = ( toggle === true ) ? 1 : 0;
   }
 
   /**
@@ -82,27 +143,29 @@ class Coin {
    */
   resolveImage() {
     if ( this.image || !this.symbol ) return;
-    const temp = new Image();
     const symb = String( this.symbol ).toLowerCase();
-    temp.addEventListener( 'load', e => { this.image = temp.src; } ); // image is set only if it loads
-    temp.src = `https://raw.githubusercontent.com/rainner/binance-watch/master/public/images/icons/${ symb }_.png`;
+    const url  = `https://www.coinrated.com/images/coins/${ symb }.svg`;
+    const deft = `https://www.coinrated.com/images/coins/default.svg`;
+    const temp = new Image();
+    // image is set only if it loads
+    temp.addEventListener( 'load', e => { this.image = temp.src; } );
+    temp.addEventListener( 'error', e => { this.image = deft; } );
+    temp.src = url;
   }
 
   /**
    * Update the 1hr graph with minute candle values
-   * @param {float}  price  last price
    */
-  updateLiveGraph( price ) {
-    price = parseFloat( price ) || 0;
-    const wait = 1; // time in secs
-    const total = 300; // time in secs
-    const now = Date.now();
+  updateLiveGraph() {
+    const wait    = 1; // time in secs
+    const total   = 180; // time in secs
+    const now     = Date.now();
     const elapsed = ( now - this.graph_last ) / 1000;
 
     if ( !this.graph_data.length ) this.fakeHistory();
     if ( elapsed < wait ) return;
 
-    this.graph_data.push( price );
+    this.graph_data.push( this.converted_price );
     this.graph_last = now;
 
     if ( this.graph_data.length > total ) {
@@ -111,16 +174,23 @@ class Coin {
   }
 
   /**
+   * Flush current live graph data
+   */
+  flushLiveGraph() {
+    this.graph_data = [];
+  }
+
+  /**
    * Come up with some fake history prices to fill in the initial line chart
    */
   fakeHistory() {
-    let num = this.price * 0.0001;
+    let num = this.converted_price * 0.0001;
     let min = -Math.abs( num );
     let max = Math.abs( num );
 
-    for ( let i = 0; i < 5; ++i ) {
+    for ( let i = 0; i < 10; ++i ) {
       let rand = Math.random() * ( max - min ) + min;
-      this.graph_data.push( this.price + rand );
+      this.graph_data.push( this.converted_price + rand );
     }
   }
 
